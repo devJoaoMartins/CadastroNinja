a4c22df1f176d018526b5ea029bb580e5ef764f4
DevJoaoMartins <joao.martins.nery@gmail.com>
2025-11-05 00:25:25 -0300

    Construindo entidade no banco de dados, apartir do NinjaModel

--- src/main/java/dev/java/CadastroNinja/Controller.java
+++
@@ -1,4 +0,0 @@
-package dev.java.CadastroNinja;
-
-public class Controller {
-}
---
+++ src/main/java/dev/java/CadastroNinja/NinjaController.java
@@ -0,0 +1,16 @@
+package dev.java.CadastroNinja;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class NinjaController {
+
+    @GetMapping("/boasVindas")
+    public String BoasVindas() {
+        return "Minha primeira mensagem";
+    }
+
+}
